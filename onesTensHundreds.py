db9aff0923306d6c5eb318ffae4fc916929ffde8
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-07-13 10:15:04 -0700

    Added a comment with an explanation of how to get the individual digits with % and //.

--- onesTensHundreds.py
+++ onesTensHundreds.py
@@ -1,3 +1,7 @@
+# Dividing by a power of 10 shifts a value right. Ex: 321 // 10 is 32. Ex: 321 // 100 is 3.
+# % by a power of 10 gets the rightmost digits. Ex: 321 % 10 is 1. Ex: 321 % 100 is 21.
+
+
 threeDigitInt = int(input("Give me a 3 digit integer: \n"))
 
 ones_digit     = threeDigitInt % 10    
