a5d8340b29253ac174f8feceaff57802901c15c1
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-07-13 09:39:28 -0700

    Created minutesToHours.py as a calculator to convert minutes to hours and minutes and  hours and minutes to minutes.

---
+++ minutesToHours.py
@@ -0,0 +1,20 @@
+
+minutesOrHours = input("For minutes to hours, type 'h'. For hours and minutes to minutes, type 'm': \n")
+
+
+if minutesOrHours == 'h':
+    minutes = int(input('Enter minutes:\n'))
+    hours = minutes // 60
+    minutes_remaining = minutes % 60
+
+    print(f'{minutes} minutes is {hours} hours and {minutes_remaining} minutes.')
+
+elif minutesOrHours == 'm':
+    hours = int(input('Enter hours:\n'))
+    minutes = int(input('Enter minutes:\n'))
+    hoursInMinutes = hours * 60
+    total = minutes + hoursInMinutes
+
+    print(f'{hours} hours and {minutes} minutes is {total} minutes.')
+    
+    
