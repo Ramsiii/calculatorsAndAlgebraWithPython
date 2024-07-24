68277c3552dcbae7bec2ae2734206edc5d5b3751
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-07-24 14:57:37 -0700

    Created get_area and a get_diameter methods with print statements in each to print out f strings with the area and diemeter respectively.

--- circleCalc.py
+++ circleCalc.py
@@ -4,16 +4,22 @@ class Circle():
     
     def __init__(self, radius = 1, unit = "cm", name="unnamed circle"):
         self.radius = radius
-        self.diameter = radius * 2
         self.unit = unit
         self.name = name
-        
+    
+    def get_diameter(self):
+        print(f"The diameter of {self.name} is {self.radius *2}.")
+           
     def get_circumference(self):
         print(f"The circumference of {self.name} is {self.radius * self.pi * 2} {self.unit}.")
+        
+    def get_area(self):
+        print(f"The area of {self.name} is {self.radius ** 2 * self.pi} square {self.unit}.")
         
 
 mycircle = Circle(3.5, "inches", "a personal pizza")
 
-print(mycircle.diameter)
 
+mycircle.get_diameter()
 mycircle.get_circumference()
+mycircle.get_area()
