ca475e3a3c87618de0472585b6d580a2e9813cc4
Ramsiii <rrimkeit@hotmail.com>
2024-09-16 16:39:45 -0700

    Created numPercentOfX.py to calculate what the whole is by inputting the part and the percent.

---
+++ numPercentOfX.py
@@ -0,0 +1,8 @@
+print("Let's find out what the whole is with only part of the whole and a percent.")
+num = float(input("Part of the whole is: "))
+percent = float(input("The percent of that whole is: "))
+
+print(f"{num} is {percent}% of {num * 100 / percent}.")
+
+
+
