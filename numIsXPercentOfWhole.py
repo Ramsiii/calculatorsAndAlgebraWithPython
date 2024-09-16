a103e4a87d5f4ff81f816e5cdcba0f2fb1cd13f3
Ramsiii <rrimkeit@hotmail.com>
2024-09-16 16:45:57 -0700

    Created numIsXPercentOfWhole.py to calculate what percent a part is of a whole.

---
+++ numIsXPercentOfWhole.py
@@ -0,0 +1,8 @@
+print("Let's find out what twhat percent a part is of a whole.")
+num = float(input("Part of the whole is: "))
+whole = float(input("The whole is: "))
+
+print(f"{num} is {(num * 100 / whole):.1f}% of {whole}.")
+
+
+
