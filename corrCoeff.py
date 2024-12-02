ce237670371defa4dce6a07e65f59cfe68babab9
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-12-02 11:49:02 -0800

    Created stack.py

---
+++ corrCoeff.py
@@ -0,0 +1,10 @@
+# TODO: creat a calculator that takes a list of tuples with x, y coordinates
+# and calculates the correlation coeefficient of these data points
+# and then calculate the coefficient of determinitation R² (r**2)
+#
+
+
+# alternatively us numpy:
+# import numpy as np
+# coords = np.array([[1,2], [3,4], [5,6]])
+# correlation = np.corrcoef(coords[:,0], coords[:,1])[0,1]
---
+++ stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def push(self, element):
+        self.elements.append(element)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.elements.pop()
+        else:
+            raise IndexError("Stack is empty.")
+
+    def is_empty(self):
+        return len(self.elements) == 0
+
+
+stack = Stack()
+
+# stack.pop()
+# print(stack.pop())
+
+stack.push(1)
+stack.push(2)
+
+print(stack.pop())
+print(stack.is_empty())
