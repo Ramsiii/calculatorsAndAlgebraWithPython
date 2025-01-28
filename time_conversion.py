0e341e368f480af70a3ccbf7bf86166a0e216ba7
Ramsey Rimkeit <rrimkeit@hotmail.com>
2025-01-27 18:27:06 -0800

    Created the time_conversion.py script to convert 12-hour time format to 24-hour time format.

---
+++ time_conversion.py
@@ -0,0 +1,25 @@
+def timeConversion(s):
+    # Write your code here
+    # if the last two char are 'AM' and
+    # the first two are '12' 
+    if s[-2:] == 'AM' and s[:2] == '12':
+        # add '00' to beginning and cut the first and last two char
+        return '00' + s[2:-2]
+    # elif last two digits are 'AM'
+    elif s[-2:] == 'PM' and s[:2] == '12':
+        return s[:-2] 
+    elif s[-2:] == 'AM':
+        # return the string without last two char 'AM'
+        return s[:-2]
+    else:
+        # if PM and not 12, add 12 to the first two char converted to an int, 
+        # convert these back to string and concatenate them with
+        # the string without the first two and the last two char
+        return str(int(s[:2]) + 12) + s[2:8]
+        
+
+if __name__ == '__main__':
+    s = input('Enter the time in the format 00:00:00AM: ')
+
+    result = timeConversion(s)
+    print(f'The 24-hour format for that time is: {result}.')
